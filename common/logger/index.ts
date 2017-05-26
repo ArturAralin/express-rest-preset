5f0ceeecc3f297315a68e627ee0814c5234707bc
Stas.Z <stas@lodossteam.com>
2017-05-26 14:27:42 +0300

    logger extended

--- common/logger/index.ts
+++ common/logger/index.ts
@@ -1,5 +1,23 @@
 import * as log4js from 'log4js';
+import { Logger } from 'log4js';
+import config from '../config';
+import { LoggerSettings } from '../../config/$config.interface';
+import { pipe, prop, equals } from 'ramda';
 
-export const createLogger = (namespace: string): log4js.Logger => {
-  return log4js.getLogger(namespace);
+const DEFAULT_LEVEL = 'all';
+
+const LOGGERS: Logger[] = [];
+const LOGGERS_CONFIGS: LoggerSettings[] = config.logger;
+
+const findConfig = (targetNamespaces: string) =>
+  pipe(prop('namespaces'), equals(targetNamespaces));
+
+export const createLogger = (loggerNamespaces: string): Logger => {
+  const logger: Logger = log4js.getLogger(loggerNamespaces);
+  const config = LOGGERS_CONFIGS.find(findConfig(loggerNamespaces)) as LoggerSettings;
+  const level = config.level.toLowerCase() || DEFAULT_LEVEL;
+
+  logger.setLevel(level);
+
+  return logger;
 };
